46cb29912f155391ddcb6cb670fb1fca5347772b
Alvaro Felipe Garcia Mendez <alvarofelipe12@gmail.com>
2025-01-20 16:31:34 -0500

    Fix image issue iOS
    - Fixed issue navigating back from detail showing the image fixed while leaving.
    - Added UT for breed card component.

--- src/app/components/breed-card/breed-card.component.spec.ts
+++ src/app/components/breed-card/breed-card.component.spec.ts
@@ -1,24 +1,93 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BreedCardComponent } from './breed-card.component';
+import { Breed } from 'src/app/models/breed.model';
+import { By } from '@angular/platform-browser';
 
-describe('BreedCardComponent', () => {
+fdescribe('BreedCardComponent', () => {
   let component: BreedCardComponent;
   let fixture: ComponentFixture<BreedCardComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ BreedCardComponent ],
-      imports: [IonicModule.forRoot()]
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [BreedCardComponent]
     }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(BreedCardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have an empty breedsList by default', () => {
+    expect(component.breedsList).toEqual([]);
+  });
+
+  it('should track by id', () => {
+    const breed: Breed = {
+      id: '1',
+      name: 'Breed 1',
+      description: '',
+      temperament: '',
+      origin: '',
+      life_span: '',
+      adaptability: 0,
+      affection_level: 0,
+      child_friendly: 0,
+      grooming: 0,
+      intelligence: 0,
+      health_issues: 0,
+      social_needs: 0,
+      stranger_friendly: 0,
+      vocalisation: 0,
+      experimental: 0,
+      hairless: 0,
+      natural: 0,
+      rare: 0,
+      rex: 0,
+      suppressed_tail: 0,
+      short_legs: 0,
+      wikipedia_url: '',
+      hypoallergenic: 0,
+      reference_image_id: '',
+      image: { url: '', id: '' },
+      weight: {
+        imperial: '',
+        metric: ''
+      },
+      country_code: '',
+      country_codes: '',
+      alt_names: '',
+      indoor: 0,
+      dog_friendly: 0,
+      energy_level: 0,
+      shedding_level: 0,
+      cfa_url: '',
+    };
+    expect(component.trackById(0, breed)).toBe('1');
+  });
+
+  it('should set default picture if image is not jpg', () => {
+    const event = {
+      target: {
+        src: 'assets/images/placeholder.jpeg'
+      }
+    } as unknown as Event;
+    component.setDefaultPic(event);
+    expect((event.target as HTMLIonImgElement).src).toBe('assets/images/placeholder.jpeg');
+  });
+
+  it('should replace jpg with png in image src', () => {
+    const event = {
+      target: {
+        src: 'assets/images/cat.jpg'
+      }
+    } as unknown as Event;
+    component.setDefaultPic(event);
+    expect((event.target as HTMLIonImgElement).src).toBe('assets/images/cat.png');
   });
 });
--- src/app/pages/breed-detail/breed-detail.page.ts
+++ src/app/pages/breed-detail/breed-detail.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Breed } from 'src/app/models/breed.model';
 import { BreedsService } from 'src/app/services/breeds.service';
@@ -9,17 +9,20 @@ import { BreedsService } from 'src/app/services/breeds.service';
   styleUrls: ['./breed-detail.page.scss'],
   standalone: false,
 })
-export class BreedDetailPage implements OnInit {
+export class BreedDetailPage {
 
   loadedBreed?: Breed;
+
+  isLeaving = false;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private breedsService: BreedsService,
     private router: Router,
+    private cdr: ChangeDetectorRef,
   ) { }
 
-  ngOnInit() {
+  ionViewWillEnter() {
     this.activatedRoute.paramMap.subscribe(paramMap => {
       if (paramMap.has('breedId')) {
         const breedId = paramMap.get('breedId')!;
@@ -30,6 +33,12 @@ export class BreedDetailPage implements OnInit {
         this.router.navigate(['/home']);
       }
     });
+  }
+
+  ionViewWillLeave() {
+    // hide the fixed ion-img item that might show during back navigation in iOS
+    this.isLeaving = true;
+    this.cdr.detectChanges();
   }
 
   setDefaultPic(event: Event) {
